3cc9f7ba1b7c49d87f6005ca91d41b17f204412d
AndersonCRF <105357863+danilokulcheski@users.noreply.github.com>
2024-04-12 07:56:09 -0300

    cvbcb

--- main.js
+++ main.js
@@ -17,7 +17,8 @@ for (let i = 0; i < botoes.length; i++) {
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-5-05T00:00:00");
 const tempoObjetivo2 = new Date("2024-6-05T00:00:00");
-const tempoObjetivo3 = new Date("2024-6-05T00:00:00");
+const tempoObjetivo3 = new Date("2025-10-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-6-05T00:00:00");
 let tempoAtual = new Date();
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
