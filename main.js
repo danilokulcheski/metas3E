922bc13f6c387896ca6c0424c80b81fb5e69cd12
AndersonCRF <105357863+danilokulcheski@users.noreply.github.com>
2024-04-12 08:29:18 -0300

    hgg

--- main.js
+++ main.js
@@ -16,10 +16,13 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo2 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo3 = new Date("2024-05-05T00:00:00");
+const tempoObjetivo4 = new Date("2024-05-05T00:00:00");
 
 
 contadores[0].textContent = calculaTempo(tempoObjetivo1);
-
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
 function calculaTempo(tempoObjetivo){
 let tempoAtual = new Date();
 let tempoFinal = tempoObjetivo - tempoAtual;
@@ -32,5 +35,9 @@ segundos %= 60;
 minutos %= 60;
 horas %= 24;
 
+if (tempoFinal > 0){
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
+}else{
+    return " PRAZO ENCERRADO!!! "
+}
 }
