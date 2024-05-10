01a72ca8308e89e037aa5cdfe4b22a1eb096b3af
AndersonCRF <105357863+danilokulcheski@users.noreply.github.com>
2024-05-10 08:27:20 -0300

    sfhdsjfdgfdfgdjhgdjgv

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ horas %= 24;
 if (tempoFinal > 0){
 return dias + " dias " + horas + " horas " + minutos + " minutos " + segundos + " segundos ";
 }else{
-    return " PRAZO ENCERRADO!!! "
+    return "PRAZO ENCERRADO!!!"
 }
 }
 
